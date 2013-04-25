88676a5f5d993ed781597ecf050604a5779fb294
Vik Ewoods <support@voroninstudio.eu>
2013-04-25 11:13:14 +0300

    Add route for contacts
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 UniAbroad::Application.routes.draw do
   match 'about-us' => 'page#about', :as => 'about', :via => 'get'
-  match 'contact-us' => 'page#about', :as => 'contact', :via => 'get'
+  match 'contact-us' => 'page#contact', :as => 'contact', :via => 'get'
   root :to => 'page#index'
 end
