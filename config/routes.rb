34dcbfc6f5a50a7e6d9c1308e6b3e58c55e92e18
Vik Ewoods <support@voroninstudio.eu>
2013-05-09 22:51:37 +0300

    Update route file
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Qualitycompass::Application.routes.draw do
-  match 'about-us' => 'page#about', :as => 'about', :via => 'get'
-  match 'contact-us' => 'page#contact', :as => 'contact', :via => 'get'
-  root :to => 'page#index'
+
+  devise_for :admins
+  mount Ckeditor::Engine => '/ckeditor'
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  scope '(:locale)', :locale => /en|ru|uk/ do
+    match 'about-us'    => 'page#about',      :as => 'about', :via => :get
+    match 'contact-us'  => 'page#contact',    :as => 'contact', :via => :get
+    match 'order'       => 'order#new',     :as => 'orders', :via => :get
+    match 'order'       => 'order#create',  :as => 'orders', :via => :post
+    match 'news'        => 'article#index', :as => 'news', :via => :get
+    match 'news/:id'        => 'article#show', :as => 'news_item', :via => :get
+    root :to            => 'page#index'
+  end
 end
