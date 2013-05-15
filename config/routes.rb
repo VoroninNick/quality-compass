67635f42abd8e6fb62508a504e3875937a780d1b
Vik Ewoods <support@voroninstudio.eu>
2013-05-16 00:34:15 +0300

    Routes for social
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Qualitycompass::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   scope '(:locale)', :locale => /en|ru|uk/ do
-    match 'about-us'    => 'page#about',      :as => 'about', :via => :get
-    match 'contact-us'  => 'page#contact',    :as => 'contact', :via => :get
-    match 'order'       => 'order#new',     :as => 'orders', :via => :get
-    match 'order'       => 'order#create',  :as => 'orders', :via => :post
-    match 'news'        => 'article#index', :as => 'news', :via => :get
-    match 'news/:id'        => 'article#show', :as => 'news_item', :via => :get
-    root :to            => 'page#index'
+    match 'about-us'                  => 'page#about',                    :as => 'about', :via => :get
+    match 'contact-us'                => 'page#contact',                  :as => 'contact', :via => :get
+    match 'order'                     => 'order#new',                     :as => 'orders', :via => :get
+    match 'order'                     => 'order#create',                  :as => 'orders', :via => :post
+    match 'news'                      => 'article#index',                 :as => 'news', :via => :get
+    match 'news/:id'                  => 'article#show',                  :as => 'news_item', :via => :get
+    match '~facebook/quality-compass'  => redirect('http://facebook.com'), :as => 'facebook'
+    match '~linkedin/quality-compass'  => redirect('http://linkedin.com'), :as => 'linkedin'
+    match '~twitter/quality-compass'   => redirect('http://twitter.com'),  :as => 'twitter'
+    match 'subscribe'                 => 'page#index',                    :as => 'subscribe'
+    root :to                          => 'page#index'
   end
 end
