cfcd0b741da4c6923ae03016e7c4f7a7ef51f7d9
Vik Ewoods <support@voroninstudio.eu>
2013-05-23 16:51:42 +0300

    Notification Mailer
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,9 +1,10 @@
 class NotificationsMailer < ActionMailer::Base
-  default :from => 'support@voroninstudio.eu'
-  default :to => 'office@qc.com'
 
-  def new_order(order)
-    @order = order
-    mail(:subject => "[YourWebsite.tld] #{order.company}")
+  default :from => 'support@voroninstudio.eu'
+  default :to => 'support@voroninstudio.eu'
+
+  def new_order(p)
+    @details = p
+    mail(:subject => "#Order request from [#{p.company}]. Message send from your website.", :from => p.email)
   end
 end
