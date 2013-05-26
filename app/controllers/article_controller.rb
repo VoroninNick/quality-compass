1073f9d900860a2eb64263b5d15ee2e0830c90ad
Vik Ewoods <support@voroninstudio.eu>
2013-05-26 21:53:42 +0300

    News MVC
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,7 +1,10 @@
 class ArticleController < ApplicationController
   def index
+    @news ||= Article.find_all_by_published(:t)
   end
 
   def show
+    @new = Article.find_by_slug!(params[:id])
+    @last_news = Article.last(4)
   end
 end
