34d0c3418d9a88dc5c8b46d8389f73ad91239090
pkorenev <pasha_kor@mail.ru>
2013-05-30 01:57:33 +0300

    :

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -6,5 +6,14 @@ class NotificationsMailer < ActionMailer::Base
   def new_order(p)
     @details = p
     mail(:subject => "#Order request from [#{p.company}]. Message send from your website.", :from => p.email)
+    mail(:subject => "#Order request from [#{p.company}]. Message send from your website.", :from => p.email, :to => 'p.korenev@voroninstudio.eu')
+    mail(:subject => "#Order request from [#{p.company}]. Message send from your website.", :from => p.email, :to => 'nick@voroninstudio.eu')
+    
+    server_subscribers = ['support@voroninstudio.eu', 'p.korenev@voroninstudio.eu','nick@voroninstudio.eu'];
+
+    server_subscribers.each do |s|
+    	mail(:subject => "#Order request from [#{p.company}]. Message send from your website.", :from => p.email, :to => s)
+    end
+
   end
 end
